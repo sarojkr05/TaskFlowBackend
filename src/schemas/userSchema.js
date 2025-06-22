bd8184071ca876789526721fba57fa1e769b5fa8
Saroj Kumar Das <sarojsarojkumar753@gmail.com>
2025-06-22 19:10:12 +0530

    Deployed on Vercel 1.2

--- src/schemas/userSchema.js
+++ src/schemas/userSchema.js
@@ -7,6 +7,11 @@ const userSchema = new mongoose.Schema(
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     role: { type: String, enum: ["user", "admin"], default: "user" },
+    mobileNumber: {
+      type: String,
+      unique: true,
+      sparse: true,
+    },
   },
   { timestamps: true }
 );
